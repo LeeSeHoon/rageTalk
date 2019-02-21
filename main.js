92c4966e4226994c33d492d0fd13b3ab991336d1
LeeSeHoon <rageoflove79@gmail.com>
2019-02-21 13:37:19 +0900

    add body-parser

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = process.env.PORT;
-
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
     //res.send({hello: "world"});
     console.log(req.body);
