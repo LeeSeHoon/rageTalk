93f1e143804540152a0d47f746448dfc988d337a
LeeSeHoon <rageoflove79@gmail.com>
2019-02-21 15:14:56 +0900

    proxy socket listen

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+const port = 9000;
+
+server.listen(port, function() {
+    console.log(`uptalk Proxy server Listen port: ${port}`);
+});
+
+io.on('connection', function(socket) {
+    console.log('uptalk Proxy server connection');
+    console.log('client socket handshake', socket.handshake);
+    console.log('client address:', socket.handshake.address);
+    
+});
+
+
+
+
 
 const PORT = process.env.PORT;
 app.use(bodyParser.json());
@@ -8,14 +28,18 @@ app.get("/", (req, res) => {
     //res.send({hello: "world"});
     console.log(req.body);
     res.sendStatus(200);
+
+    io.emit('nv_event' , req.body);
 });
 
 
 
-app.post("/", (req, res) => {
+app.post("/NV", (req, res) => {
     //res.send({hello: "world"});
     console.log(req.body);
     res.sendStatus(200);
+
+    io.emit('nv_event' , req.body);
 });
 
 app.listen(PORT || 8080);
