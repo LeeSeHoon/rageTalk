d2022caa4f2d4f4702c2701a7c81aa3810011dba
LeeSeHoon <rageoflove79@gmail.com>
2019-02-21 10:44:24 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send({hello: "world"});
+});
+
+app.listen(PORT || 8080);
+
